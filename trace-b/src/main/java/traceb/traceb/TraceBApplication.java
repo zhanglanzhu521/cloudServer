23cab569b8e34ab594af9865fe17115e2585fbde
512312126@qq.com <zlz1987518>
2018-08-09 18:37:40 +0800

    add 2018年8月8日10:17:20

---
+++ trace-b/src/main/java/traceb/traceb/TraceBApplication.java
@@ -0,0 +1,25 @@
+package traceb.traceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@SpringBootApplication
+public class TraceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceBApplication.class, args);
+	}
+
+	@GetMapping("/trace-b")
+	public Mono<String> trace() {
+		System.out.println("===call trace-b===");
+
+		return Mono.just("Trace");
+	}
+
+
+}
